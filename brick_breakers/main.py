1963037e4d38dff7d8ea819379d1d05db258b447
1304PK <pushkark2005@gmail.com>
2025-04-18 01:21:33 +0530

    change

--- brick_breakers/main.py
+++ brick_breakers/main.py
@@ -1,1 +1,14 @@
+import pygame
 
+pygame.init()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()

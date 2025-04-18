7b5721f5b33ecd1ec8f60a3e7b5183ac9cdd16cd
1304PK <pushkark2005@gmail.com>
2025-04-18 22:55:43 +0530

    Add the elements

--- brick_breakers/main.py
+++ brick_breakers/main.py
@@ -2,8 +2,17 @@ import pygame
 
 pygame.init()
 
-width, height = 800, 600
+width, height = 1200, 900
 screen = pygame.display.set_mode((width, height))
+
+# rect = pygame.Rect(0, 0, 118, 30)
+rects = []
+for row in range(0, 8):
+    for column in range(0, width, 120):
+        rect = pygame.Rect(column, 30*row, 118, 28)
+        rects.append(rect)
+    
+paddle = pygame.Rect(0, height-20, 120, 20)
 
 running = True
 while running:
@@ -11,5 +20,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # pygame.draw.rect(screen, (255, 255, 255), rect)
+    for rect in rects:
+        pygame.draw.rect(screen, (255, 255, 255), rect)
+    pygame.draw.rect(screen, (255, 255, 255), paddle)
+    pygame.draw.circle(screen, (255, 255, 255), (width/2, height/2), 10)
+    pygame.display.update()
 
 pygame.quit()

b6587e5dc7fca69306db16387766f0c31d57f759
1304PK <pushkark2005@gmail.com>
2025-04-25 01:14:59 +0530

    Add intro screen before main game

--- brick_breakers/main.py
+++ brick_breakers/main.py
@@ -6,6 +6,7 @@ pygame.init()
 width, height = 600, 600
 screen = pygame.display.set_mode((width, height))
 
+# ==================  VARIABLES  ================== #
 ballx = width/2
 bally = height/2
 paddlevel = 13
@@ -14,13 +15,16 @@ vely = -4
 run = False
 font = pygame.font.SysFont('Clash', 30)
 score = 0
+isPlaying = False
 
 winStatement = pygame.transform.scale(pygame.image.load('brick_breakers/Assets/win.png'), (462, 264))
 winStatement_rect = winStatement.get_rect(topleft = (width/2 - 231, 305))
+clock = pygame.time.Clock()
+running = True
 
 
-# rect = pygame.Rect(0, 0, 118, 30) rectangle
-brick = pygame.transform.scale(pygame.image.load("brick_breakers/Assets/brick1.png"), (70, 35))
+# ================== IMAGES ================== #
+brick = pygame.transform.scale(pygame.image.load("brick_breakers/Assets/brick.png"), (70, 35))
 rects = []
 for row in range(2, 6):
     for column in range(0, width, 70):
@@ -29,11 +33,14 @@ for row in range(2, 6):
         rects.append(rect)
 
 paddle_img = pygame.image.load("brick_breakers/Assets/paddle.jpg")
-paddle = paddle_img.get_rect(topleft = (0, height - 12))
+paddle = paddle_img.get_rect(topleft = (width/2 - 60, height - 12))
 # paddle = pygame.Rect(0, height-20, 120, 20)
 ball = pygame.Rect(width/2-10, height/2-10, 20, 20)
+introScreen = pygame.transform.scale(pygame.image.load('brick_breakers/Assets/intro_screen.png'), (width, height))
+lives = pygame.Rect(500, 15, 20, 20)
 
 
+# ================== FUNCTIONS ================== #
 def updateScore():
     with open("brick_breakers/score.txt", "r") as file:
         max_score = file.read()
@@ -44,69 +51,70 @@ def updateScore():
 
 def collision():
     global velx, vely, rects, score
-    if paddle.colliderect(ball) or ball.y < 62 or ball.bottom >= height-200:
+    if paddle.colliderect(ball) or ball.y < 62 or ball.bottom >= height:
         vely *= -1
     if ball.left == 0 or ball.right == width:
         velx *= -1
 
     for rect in rects:
         if ball.colliderect(rect):
-
-            # if velx > 0 and ball.bottom < rect.bottom:
-            #     velx *= -1
-            # elif velx < 0 and ball.bottom < rect.bottom:
-            #     velx *= -1
-            # vely *= -1
             if ball.left - rect.left >= -10 and ball.right-rect.right<=10:
                 vely *= -1
             if abs(ball.top-rect.top)<10 or abs(ball.bottom - rect.bottom)<10:
                 velx *= -1
-            print(ball.right-rect.right)
-                
             score += 1
             rects.remove(rect)
         
 
-clock = pygame.time.Clock()
-running = True
+# ==================== GAME LOOP ==================== #
 
 while running:
-    clock.tick(10)
-    score_text = font.render(f'{score}', True, (255, 255, 255))
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                run = True
+            if event.key == pygame.K_RETURN:
+                isPlaying = True
+        
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a] and paddle.x >= 3:
-        paddle.x -= paddlevel
-    elif keys[pygame.K_d] and paddle.x <= width-122:
-        paddle.x += paddlevel
+    
 
-    if run:
+    if isPlaying:
         ball.x += velx
         ball.y += vely
         collision()
 
-    screen.fill((37, 32, 41))
-    for rect in rects:
-        # pygame.draw.rect(screen, (255, 255, 255), rect)
-        screen.blit(brick, rect)
-    # pygame.draw.rect(screen, (255, 255, 255), paddle)
-    screen.blit(paddle_img, paddle)
-    pygame.draw.circle(screen, (255, 255, 255), ball.center, 10)
-    pygame.draw.line(screen, (255, 255, 255), (0, 60), (width, 60))
-    screen.blit(score_text, (width/2-score_text.get_width(), 12))
-    if len(rects) == 0:
-        screen.blit(winStatement, winStatement_rect)
-        velx, vely = 0, 0
-        updateScore()
-    
-    # pygame.draw.rect(screen, (255, 0, 0), ball, 1)
+        score_text = font.render(f'{score}', True, (255, 255, 255))
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and paddle.x >= 3:
+            paddle.x -= paddlevel
+        elif keys[pygame.K_d] and paddle.x <= width-122:
+            paddle.x += paddlevel
+
+        screen.fill((37, 32, 41))
+        for rect in rects:
+            screen.blit(brick, rect)
+        screen.blit(paddle_img, paddle)
+        pygame.draw.circle(screen, (255, 255, 255), ball.center, 10)
+        pygame.draw.circle(screen, (255, 255, 255), (500, 30), 10, 1)
+        pygame.draw.circle(screen, (255, 255, 255), (525, 30), 10)
+        pygame.draw.circle(screen, (255, 255, 255), (550, 30), 10)
+        pygame.draw.line(screen, (255, 255, 255), (0, 60), (width, 60))
+        screen.blit(score_text, (width/2-score_text.get_width(), 12))
+        # pygame.draw.circle(screen, (255, 255, 255), ball.centre, 10)
+        
+        if len(rects) == 0:
+            screen.blit(winStatement, winStatement_rect)
+            velx, vely = 0, 0
+            updateScore()
+
+    else:
+        screen.blit(introScreen, (0, 0))
+        
+        # pygame.draw.rect(screen, (255, 0, 0), ball, 1)
     pygame.display.flip()
 
 pygame.quit()
